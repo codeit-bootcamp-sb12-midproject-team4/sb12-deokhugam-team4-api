47186b4b00dbe5e60a7cd19ee9c9f929d706931d
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-06-26 17:03:12 +0900

    feat/알림 서비스 인터페이스 정의

---
+++ src/main/java/com/codeit/deokhugam/domain/notification/service/NotificationService.java
@@ -0,0 +1,34 @@
+package com.codeit.deokhugam.domain.notification.service;
+
+import com.codeit.deokhugam.domain.common.CursorPageResponse;
+import com.codeit.deokhugam.domain.notification.dto.NotificationResponse;
+import com.codeit.deokhugam.domain.notification.event.CommentCreatedEvent;
+import com.codeit.deokhugam.domain.notification.event.PopularReviewSelectedEvent;
+import com.codeit.deokhugam.domain.notification.event.ReviewLikedEvent;
+import java.time.Instant;
+import java.util.UUID;
+import org.springframework.data.domain.Sort.Direction;
+
+public interface NotificationService {
+
+  void saveCommentCreatedNotification(CommentCreatedEvent event);
+
+  void saveReviewLikedNotification(ReviewLikedEvent event);
+
+  void savePopularReviewSelectedNotification(PopularReviewSelectedEvent event);
+
+  NotificationResponse markNotificationAsRead(
+      UUID notificationId,
+      UUID requestUserId
+  );
+
+  void markAllNotificationsAsRead(UUID userId);
+
+  CursorPageResponse<NotificationResponse> findByUserId(
+      UUID userId,
+      Direction direction,
+      String cursor,
+      Instant after,
+      int limit
+  );
+}
