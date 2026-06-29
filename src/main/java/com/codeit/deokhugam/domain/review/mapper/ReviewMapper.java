8cc47c19691bb42b6988bf67d8cbfface16cadbf
ozingeodari <ozingeodari@gmail.com>
2026-06-29 14:44:51 +0900

    feat: ReviewService 구현 - Review, ReviewLike: Builder 추가 - Review: update 메소드 수정

--- src/main/java/com/codeit/deokhugam/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/codeit/deokhugam/domain/review/mapper/ReviewMapper.java
@@ -4,11 +4,27 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.codeit.deokhugam.domain.book.Book;
 import com.codeit.deokhugam.domain.review.Review;
+import com.codeit.deokhugam.domain.review.dto.ReviewCreateRequest;
 import com.codeit.deokhugam.domain.review.dto.ReviewResponse;
+import com.codeit.deokhugam.domain.user.User;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
+
+	@Mapping(target = "book", source = "book")
+	@Mapping(target = "user", source = "user")
+	@Mapping(target = "content", source = "request.content")
+	@Mapping(target = "rating", source = "request.rating")
+	@Mapping(target = "likeCount", ignore = true)
+	@Mapping(target = "commentCount", ignore = true)
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "createdAt", ignore = true)
+	@Mapping(target = "updatedAt", ignore = true)
+	@Mapping(target = "deletedAt", ignore = true)
+	@Mapping(target = "attachmentUrl", ignore = true)
+	Review toEntity(ReviewCreateRequest request, Book book, User user);
 
 	@Mapping(target = "bookId", source = "review.book.id")
 	@Mapping(target = "bookTitle", source = "review.book.title")
