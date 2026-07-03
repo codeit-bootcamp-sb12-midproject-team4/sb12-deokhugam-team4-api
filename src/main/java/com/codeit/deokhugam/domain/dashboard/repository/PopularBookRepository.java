485f4c3497560ec1839b8c4987be426f290222e0
suaripa <kimsua9090@gmail.com>
2026-07-03 15:37:28 +0900

    대시보드 조회

--- src/main/java/com/codeit/deokhugam/domain/dashboard/repository/PopularBookRepository.java
+++ src/main/java/com/codeit/deokhugam/domain/dashboard/repository/PopularBookRepository.java
@@ -12,9 +12,11 @@ import com.codeit.deokhugam.domain.dashboard.entity.PeriodType;
 import com.codeit.deokhugam.domain.dashboard.entity.PopularBook;
 
 public interface PopularBookRepository extends JpaRepository<PopularBook, UUID> {
-
-    // [조회] 대시보드 랭킹 화면 출력 전용
-    List<PopularBook> findByPeriodAndBatchDateOrderByRankingAsc(PeriodType period, LocalDate batchDate);
+    
+    List<PopularBook> findByPeriodAndBatchDateOrderByRankingAsc(
+        PeriodType period,
+        LocalDate batchDate
+    );
 
     // [배치] JPQL 없이 Spring Data JPA 컨벤션으로 처리하는 벌크 삭제
     @Modifying(clearAutomatically = true, flushAutomatically = true)
