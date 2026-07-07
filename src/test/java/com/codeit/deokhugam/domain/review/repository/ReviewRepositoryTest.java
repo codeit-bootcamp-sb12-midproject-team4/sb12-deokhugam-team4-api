0b06483dd3a4a8c262a52732965755898760205a
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-07-07 14:12:54 +0900

    fix: Book 엔티티 수정에 따른 빌더 수정

--- src/test/java/com/codeit/deokhugam/domain/review/repository/ReviewRepositoryTest.java
+++ src/test/java/com/codeit/deokhugam/domain/review/repository/ReviewRepositoryTest.java
@@ -68,7 +68,7 @@ class ReviewRepositoryTest {
 			.publisher("아니지롱")
 			.publishedDate(LocalDate.of(2026, 1, 1))
 			.isbn("12345")
-			.thumbnailUrl("url")
+			.thumbnailKey("thumbnail-key")
 			.reviewCount(0L)
 			.rating(0.0)
 			.build());
@@ -79,7 +79,7 @@ class ReviewRepositoryTest {
 			.publisher("아니지롱")
 			.publishedDate(LocalDate.of(2026, 1, 2))
 			.isbn("23456")
-			.thumbnailUrl("url")
+			.thumbnailKey("thumbnail-key")
 			.reviewCount(0L)
 			.rating(0.0)
 			.build());
@@ -90,7 +90,7 @@ class ReviewRepositoryTest {
 			.publisher("실화")
 			.publishedDate(LocalDate.of(2026, 1, 3))
 			.isbn("34567")
-			.thumbnailUrl("url")
+			.thumbnailKey("thumbnail-key")
 			.reviewCount(0L)
 			.rating(0.0)
 			.build());
