52662f59be49e26b1b6cf90012634a9b3c874c5f
kyle081 <tiamo220@gmail.com>
2026-07-07 14:32:02 +0900

    refactor: CommentException DeokhugamException 상속으로 변경

--- src/main/java/com/codeit/deokhugam/domain/comment/exception/CommentException.java
+++ src/main/java/com/codeit/deokhugam/domain/comment/exception/CommentException.java
@@ -4,11 +4,8 @@ import com.codeit.deokhugam.global.exception.DeokhugamException;
 import com.codeit.deokhugam.global.exception.ErrorCode;
 
 public class CommentException extends DeokhugamException {
+
 	public CommentException(ErrorCode errorCode) {
 		super(errorCode);
-	}
-
-	public CommentException(ErrorCode errorCode, Throwable cause) {
-		super(errorCode, cause);
 	}
 }
